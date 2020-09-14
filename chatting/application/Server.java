8ebc98a65c9cf91e7e202b25245fa7563d89b900
Abhishek51116 <abhishekaggarwal00@gmail.com>
2020-09-15 01:01:24 +0530

    initial commit

---
+++ chatting/application/Server.java
@@ -0,0 +1,17 @@
+
+package chatting.application;
+
+import javax.swing.*;
+
+public class Server extends JFrame {
+    Server(){
+        
+    setSize(500,600);
+    setLocation(400,200);
+    setVisible(true);
+    }
+    
+    public static void main(String[] args){
+        new Server();
+    }
+}
